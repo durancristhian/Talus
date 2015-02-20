e2ccf3a82c4567f64d49bd89cebcea687b773cdf
Duran Cristhian <durancristhian@gmail.com>
2015-02-20 15:39:57 -0300

    New structure of code + initial animation + little js

---
+++ chrome-extension/js/app.js
@@ -0,0 +1,34 @@
+$(document).on("ready", init);
+
+function init () {
+
+	console.log("Hi developers (:");
+
+	GetData();
+}
+
+function GetData () {
+
+	$.getJSON("http://api.openweathermap.org/data/2.5/weather?q=Buenos+Aires&units=metric&lang=es")
+		.done(function getJSONDoneCallback (data) {
+
+			UpdateUI(data);
+		})
+		.fail(function getJSONFailCallback () {
+
+			console.log("Error :(");
+		});
+}
+
+function UpdateUI (data) {
+
+	$(".current-weather__place-city").text(data.name);
+	$(".current-weather__place-country").text(data.sys.country);
+	$(".current-weather__temp-value").text(data.main.temp.toPrecision(2));
+	$(".current-weather__temp-min-value").text(data.main.temp_min);
+	$(".current-weather__temp-max-value").text(data.main.temp_max);
+	$(".current-weather__humidity").text(data.main.humidity);
+	$(".current-weather__wind").text(data.wind.speed);
+
+	new WOW().init();
+}
