b66beab52bde30924f1dbe72baadca02a88850fa
Duran Cristhian <durancristhian@gmail.com>
2015-02-21 21:30:15 -0300

    New UI

--- chrome-extension/js/app.js
+++ chrome-extension/js/app.js
@@ -23,12 +23,7 @@ function GetData () {
 function UpdateUI (data) {
 
 	// Review this values
-	$(".current-weather__place-city").text(data.name);
-	$(".current-weather__place-country").text(data.sys.country);
-	$(".current-weather__temp-value").text(data.main.temp.toPrecision(2));
-	$(".current-weather__temp-min-value").text(data.main.temp_min.toPrecision(2));
-	$(".current-weather__temp-max-value").text(data.main.temp_max.toPrecision(2));
-	$(".current-weather__update-time-value").text(moment().fromNow());
+	$(".current-weather__temp").text(data.main.temp.toPrecision(2));
 
 	// Init animation of .current-weather box
 	new WOW().init();
