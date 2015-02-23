640a70c9e45290f2fa18c7cb1a9512ea7f963f4f
Duran Cristhian <durancristhian@gmail.com>
2015-02-23 18:19:11 -0300

    Finished UI + app.js + images + new font

--- chrome-extension/js/app.js
+++ chrome-extension/js/app.js
@@ -1,3 +1,14 @@
+var forecastsWeatherUrl = "http://api.openweathermap.org/data/2.5/forecast/daily?q=Buenos+Aires&units=metric&lang=es&cnt=6";
+var todaysWeatherUrl = "http://api.openweathermap.org/data/2.5/weather?q=Buenos+Aires&units=metric&lang=es";
+var weekday = new Array(7);
+weekday[0]= "Lunes";
+weekday[1]= "Martes";
+weekday[2]= "Miércoles";
+weekday[3]= "Jueves";
+weekday[4]= "Viernes";
+weekday[5]= "Sábado";
+weekday[6]= "Domingo";
+
 $(document).on("ready", init);
 
 function init () {
@@ -9,22 +20,61 @@ function init () {
 
 function GetData () {
 
-	$.getJSON("http://api.openweathermap.org/data/2.5/weather?q=Buenos+Aires&units=metric&lang=es")
-		.done(function getJSONDoneCallback (data) {
+	$.getJSON(todaysWeatherUrl)
+		.done(function (data) {
 
-			UpdateUI(data);
+			UpdateMainInfo(data);
 		})
-		.fail(function getJSONFailCallback () {
+		.fail(function () {
 
-			console.log("Error :(");
+			showError();
+		});
+
+	$.getJSON(forecastsWeatherUrl)
+		.done(function (data) {
+
+			UpdateForecastBoxes(data);
+		})
+		.fail(function () {
+
+			showError();
 		});
 }
 
-function UpdateUI (data) {
+function UpdateMainInfo (data) {
 
-	// Review this values
-	$(".current-weather__temp").text(data.main.temp.toPrecision(2));
+	$(".place").text(data.name + ", " + data.sys.country);
 
-	// Init animation of .current-weather box
+	var imageName = data.weather[0].icon;
+	var today = $(".box.today");
+	today.find(".box__content").text(data.main.temp.toPrecision(2) + "°");
+	today.find(".box__image").append("<img src='http://openweathermap.org/img/w/" + imageName + ".png' alt='weather icon'>");
+	today.addClass("wow fadeInDown");
+
 	new WOW().init();
+}
+
+function UpdateForecastBoxes (data) {
+
+	var forecastBoxes = $(".box:not(.today)");
+
+	$.each(data.list, function (index, value) {
+
+		var box = forecastBoxes[index];
+		var imageName = value.weather[0].icon;
+
+		$(box).find(".box__title").text(weekday[index + 1]);
+		$(box).find(".box__image").append("<img src='http://openweathermap.org/img/w/" + imageName + ".png' alt='weather icon'>");
+		$(box).find(".box__content").text(value.temp.day.toPrecision(2) + "°");
+		$(box).addClass("wow fadeInDown");
+	});
+
+	new WOW().init();
+}
+
+function showError () {
+
+	$(".app").hide();
+	$(".error").fadeIn("slow");
+	console.log("Error :(");
 }
